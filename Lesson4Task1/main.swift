d0e9c3d80c49da0fe686cfc3d32d4bd332ac89c2
Kirill Netavskiy <simu4.net@gmail.com>
2016-11-09 20:19:50 +0200

    no message

--- Lesson4Task1/main.swift
+++ Lesson4Task1/main.swift
@@ -8,5 +8,32 @@
 
 import Foundation
 
-print("Hello, World!")
 
+let firstPlayerIndex = 0;
+let secondPlayerIndex = 1;
+let playersInTeamCount = 2;
+
+
+var players = ["Ukraine", "Russia", "USA", "China", "Italy", "France", "Spain", "Germany", "England", "Ireland", "Norway", "Holland", "Poland", "Netherlands", "Iceland", "Portugal"]
+
+
+//players.count
+
+
+var teams = [[String]](repeating:[String](), count:players.count/playersInTeamCount)
+
+
+for player in players {
+    var isPlayerInTeam = false
+    while (!isPlayerInTeam) {
+        var randomTeamIndex = Int(arc4random_uniform(UInt32(players.count/playersInTeamCount)))
+        if teams[randomTeamIndex].count < playersInTeamCount {
+            teams[randomTeamIndex].append(player)
+            isPlayerInTeam = true
+            //          teams.dropLast()
+        }
+    }
+}
+
+
+print(teams)
